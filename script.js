4fb2b29d93e7d390b57a4befc848e4acbc4032ff
jersonb <jersonlbrito@hotmail.com>
2023-09-10 03:13:02 -0300

    finish

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function setHref() {
+    let i=0;
     document
     .querySelectorAll('.next-page')
     .forEach(element => {
+         element.setAttribute('id', i++);
          element.setAttribute('href', `#${parseInt(element.getAttribute('id'))+1}`);
     });
 }
